b9222d65285d427c42d077636f0d5bca8035e54d
Roger Holden <r.holden@chi.ac.uk>
2020-04-09 18:36:20 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("I want to sync this to rholden2 GH account then allow rholden to fork / access it.");
+    }
+}
