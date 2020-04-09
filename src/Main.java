3ae94d93c533e3903eea513a6a7662658f6e6316
Roger Holden <r.holden@chi.ac.uk>
2020-04-09 20:24:12 +0100

    Comment added by tutor

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("I want to sync this to rholden2 GH account then allow rholden to fork / access it.");
+
+//        RH - the problem is above. It's crap.
     }
+
+
 }
